25dd8db47b3d037aa159dfde2ab182fe9dbf48ce
daniel-dubon-dev <danieldubonxd@gmail.com>
2024-07-09 14:58:35 -0600

    feat/clerk-auth-design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,41 @@
-import './globals.css'
-import { Inter as FontSans } from 'next/font/google'
-
-import { cn } from '@/lib/utils'
-import { ThemeProvider } from '../components/theme-provider'
+import "./globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "../components/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/toggle-dark-mode";
 
 const fontSans = FontSans({
-	subsets: ['latin'],
-	variable: '--font-sans'
-})
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function RootLayout({
-	children
+  children,
 }: Readonly<{
-	children: React.ReactNode
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en' suppressHydrationWarning>
-			<head />
-			<body
-				className={cn('min-h-screen font-sans antialiased', fontSans.variable)}
-			>
-				<ThemeProvider
-					attribute='class'
-					defaultTheme='system'
-					enableSystem
-					disableTransitionOnChange
-				>
-					{children}
-				</ThemeProvider>
-			</body>
-		</html>
-	)
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ModeToggle />
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import { ModeToggle } from "../components/toggle-dark-mode";
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -9,6 +15,12 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -16,7 +28,6 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-          <ModeToggle />
             By{" "}
             <Image
               src="/vercel.svg"
