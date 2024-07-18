ddc4025c9e5d6f04f22901f1ae830ed45031e47f
daniel-dubon-dev <danieldubonxd@gmail.com>
2024-07-18 13:16:03 -0600

    feat/content-slug

--- app/dashboard/_components/card-list.tsx
+++ app/dashboard/_components/card-list.tsx
@@ -11,6 +11,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { FocusIcon } from "lucide-react";
+import Link from "next/link";
 
 type Tool = {
   name: string;
@@ -28,47 +29,49 @@ type CardDemoProps = {
 
 export function CardDemo({ className, tool, ...props }: CardDemoProps) {
   return (
-    <Card
-      className={cn(
-        "w-[310px] hover:cursor-pointer border-[1px] dark:border-[#2c2c2c] border-[#6d6d6d] md:w-auto max-w-[370px]",
-        className
-      )}
-      {...props}
-    >
-      <CardHeader>
-        <CardTitle>{tool.name}</CardTitle>
-        <CardDescription className="text-primary">
-          {tool.desc}
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className=" flex items-center space-x-4 rounded-md border p-4">
-          <FocusIcon /> 
-          {/* <img className="w-7 h-7" src={tool.icon} alt={tool.desc} /> */}
-          <div className="flex-1 space-y-1">
-            <p className="text-sm font-medium leading-none">
-              Category: {tool.category}
-            </p>
-            <p className="text-sm text-muted-foreground">
-              ¡Try now! and see the magic...
-            </p>
-          </div>
-        </div>
-        <div>
-          <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
-            <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-            <div className="space-y-1">
-              <p className="text-sm font-medium leading-none">{tool.slug}</p>
-              <p className="text-sm text-muted-foreground">{tool.aiPrompt}</p>
+    <Link href={`/dashboard/content/${tool.slug}`}>
+      <Card
+        className={cn(
+          "w-[310px] hover:cursor-pointer border-[1px] dark:border-[#2c2c2c] border-[#6d6d6d] md:w-auto max-w-[370px]",
+          className
+        )}
+        {...props}
+      >
+        <CardHeader>
+          <CardTitle>{tool.name}</CardTitle>
+          <CardDescription className="text-primary">
+            {tool.desc}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className=" flex items-center space-x-4 rounded-md border p-4">
+            <FocusIcon />
+            {/* <img className="w-7 h-7" src={tool.icon} alt={tool.desc} /> */}
+            <div className="flex-1 space-y-1">
+              <p className="text-sm font-medium leading-none">
+                Category: {tool.category}
+              </p>
+              <p className="text-sm text-muted-foreground">
+                ¡Try now! and see the magic...
+              </p>
             </div>
           </div>
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">
-          <MagicWandIcon className="mr-2 h-4 w-4" /> Generate magic!
-        </Button>
-      </CardFooter>
-    </Card>
+          <div>
+            <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
+              <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+              <div className="space-y-1">
+                <p className="text-sm font-medium leading-none">{tool.slug}</p>
+                <p className="text-sm text-muted-foreground">{tool.aiPrompt}</p>
+              </div>
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">
+            <MagicWandIcon className="mr-2 h-4 w-4" /> Generate magic!
+          </Button>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 }
--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Form } from "../../_components/form";
+import { Output } from "../../_components/output";
 
 const CreateNewContent = () => {
-  return <div>CreateNewContent</div>;
+  return (
+    <div className="flex flex-col justify-center items-center rounded-sm">
+      <Form />
+      <Output />
+    </div>
+  );
 };
 
 export default CreateNewContent;
