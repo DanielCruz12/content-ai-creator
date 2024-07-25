95d4005d1b4912b5fa957ed8483e3f7299716710
daniel-dubon-dev <danieldubonxd@gmail.com>
2024-07-25 09:25:22 -0600

    feat/supabase

--- lib/supabase/migrate.ts
+++
@@ -1,7 +0,0 @@
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
-import { connection, db } from './db';
-
-(async () => {
-    await migrate(db, { migrationsFolder: './migrations' });
-    await connection.end();
-})();
--- migrations/0001_first_energizer.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "Aitemplates" ADD COLUMN "test" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "formFields" ADD COLUMN "input" text;
