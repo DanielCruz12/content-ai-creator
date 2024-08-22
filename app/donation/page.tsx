c282c8922cc786ee66e7839ddc996b1724b47b2c
dandev <danieldubonxd@gmail.com>
2024-08-22 17:22:52 -0600

    feat/dashboard, donation page, new packages installed

--- app/donation/page.tsx
+++ app/donation/page.tsx
@@ -1,11 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import { FC, useState } from "react";
+import { useState } from "react";
 
-type DonatePageProps = {
-  imgSize: string;
-};
-const DonatePage: FC<DonatePageProps> = ({ imgSize = "w-full h-full" }) => {
+const DonatePage = ({ imgSize }: any) => {
   const [copied, setCopied] = useState(false);
   const walletAddress =
     "lnbc1pnv08hxpp5srcmv0fxhmlz6yneraxgcgez632q7q6czwkhkukanjunvwz5v2ysdp82pshjgr5dusyymrfde4jq4mpd3kx2apq24ek2uscqzpuxqyz5vqsp5hpg6zyxu6ykr52p6l3x7n3wzy367lfvay47lfr3mj9ulyk6kf9sq9qxpqysgq42dhp6krefyhk7pxd7qcw4w08lkpnvcy9uzcn6lqd9ljf2zklpjjxtj4enc2u6q02pl26wcpz474rqjtwkv4uynx6g6j38w8du3jspgq64lj2q";
@@ -29,7 +26,11 @@ const DonatePage: FC<DonatePageProps> = ({ imgSize = "w-full h-full" }) => {
           contribution is greatly appreciated!
         </p>
         <div className="flex justify-center mt-6">
-          <img src="/image.png" alt="QR Code" className={imgSize} />
+          <img
+            src="/image.png"
+            alt="QR Code"
+            className={imgSize ? imgSize : "w-full h-full"}
+          />
         </div>
         <div className="mt-6 text-center">
           <p className="text-gray-700">Or send to my wallet address:</p>
