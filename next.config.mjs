fb822de88c43a498cb3fdb2fed2c26301cc827f5
dandev <danieldubonxd@gmail.com>
2024-09-30 21:39:09 -0600

    feat/image-patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
-    domains: ["dandevcreator.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fastly.picsum.photos",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+      {
+        protocol: "https",
+        hostname: "dandevcreator.com",
+      },
+    ],
   },
 };
 
