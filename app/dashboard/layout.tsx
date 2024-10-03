11c06e2191c46c1e3f76ee6a0f0edf6d7a9c1859
DanielCruz <94976934+DanielCruz12@users.noreply.github.com>
2024-10-02 21:19:31 -0600

    Update layout.tsx
    Specific sign out url

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
             <ModeToggle />
           </div>
           <SignedIn>
-            <UserButton />
+            <UserButton afterSignOutUrl="/" />
           </SignedIn>
         </header>
 
