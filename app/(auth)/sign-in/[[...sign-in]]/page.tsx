fca5db76d611813d94336e50f1a977cc922462c7
dandev <danieldubonxd@gmail.com>
2024-12-08 21:34:53 -0600

    sign-in

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,11 @@
-/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <SignIn />
+    <div className="flex justify-center items-center h-screen">
+      <SignIn />
+    </div>
   );
 }
