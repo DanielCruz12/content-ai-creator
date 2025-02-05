4227140f08cd43165c25d6e760f5ab95718ad06a
dandev <danieldubonxd@gmail.com>
2025-02-05 13:33:34 -0600

    test error

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,5 +2,6 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
+  const a = 1
   return twMerge(clsx(inputs))
 }
